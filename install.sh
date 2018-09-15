729241802145298f8f5c2cb9fceecfb9c43a3e0c
Yihui He 何宜晖 <yihuihe@foxmail.com>
2018-09-14 20:58:29 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,3 +3,8 @@ sudo dpkg -i cuda-repo-ubuntu1604_8.0.44-1_amd64.deb
 sudo apt-key adv --fetch-keys http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/7fa2af80.pub
 sudo apt-get update
 sudo apt-get install cuda
+
+sudo tar -xvf cudnn-9.2-linux-x64-v7.2.1.38.tar
+sudo cp cuda/include/cudnn.h /usr/local/cuda/include
+sudo cp cuda/lib64/libcudnn* /usr/local/cuda/lib64
+sudo chmod a+r /usr/local/cuda/include/cudnn.h /usr/local/cuda/lib64/libcudnn*
