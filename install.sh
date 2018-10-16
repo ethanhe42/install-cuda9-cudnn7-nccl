6b3cb1fe146183e3d482c5465afd3d565157f1c1
yihui-he <yihuihe@foxmail.com>
2018-10-15 21:04:26 -0400

    cu9

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@ sudo dpkg --purge cuda-repo-ubuntu1404
 sudo dpkg -i cuda-repo-ubuntu1604_9.0.176-1_amd64.deb
 sudo apt-key adv --fetch-keys http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/7fa2af80.pub
 sudo apt-get update
-sudo apt-get install -y cuda
+sudo apt-get install -y cuda-9.0
 
 wget https://github.com/yihui-he/install-cuda9-cudnn7-nccl/releases/download/tar/cudnn-9.0-linux-x64-v7.3.1.20.tgz
 tar -xzvf cudnn-9.0-linux-x64-v7.3.1.20.tgz
