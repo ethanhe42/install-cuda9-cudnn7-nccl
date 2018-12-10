2fa6a33d0d0531d621b4c378094296de965e1037
yihui-he <yihuihe@foxmail.com>
2018-12-10 14:15:39 -0500

    great

---
+++ cudnn.sh
@@ -0,0 +1,3 @@
+sudo cp cuda/include/cudnn.h /usr/local/cuda/include
+sudo cp cuda/lib64/libcudnn* /usr/local/cuda/lib64
+sudo chmod a+r /usr/local/cuda/include/cudnn.h /usr/local/cuda/lib64/libcudnn*
